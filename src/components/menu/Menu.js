d5e6f133529fbe0b8a4e3c6386b99456f25da727
Orgil <orgil.dorj720@gmail.com>
2021-03-28 22:58:40 +0200

    Better Header

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,20 +1,40 @@
-import React from 'react'
-import styles from "./Menu.module.css"
+/** @format */
+
+import React from "react";
+import styles from "./Menu.module.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function Menu() {
-    return (
-        <div className={styles.menuContainer}>
-            <ul className={styles.menuList}>
-                <li>Нүүр</li>
-                <li>Бидний тухай</li>
-                <li>Мэдээ</li>
-                <li>Холбоо барих</li>
-            </ul>
-            <div className={styles.enMenu}>
-                English
-            </div>
+  return (
+    <Router>
+      <div className={styles.menuContainer}>
+        <div className={styles.menuList}>
+          <ul>
+            <li>
+              <Link to='/'>Нүүр</Link>
+            </li>
+            <li>
+              <Link to='/about'>Бидний тухай</Link>
+            </li>
+            <li>
+              <Link to='/news'>Мэдээ</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Холбоо барих</Link>
+            </li>
+          </ul>
         </div>
-    )
+        <div className={styles.enMenu}>
+          <Link>English</Link>
+        </div>
+        <Switch>
+          <Route path='/about'></Route>
+          <Route path='/users'></Route>
+          <Route path='/'></Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default Menu
+export default Menu;
