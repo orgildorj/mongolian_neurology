3e50f5aeafed8f5947a76d183eac141a1b0efa28
Zorig <zorigdorj@gmail.com>
2021-04-02 22:51:28 +0200

    add server

--- src/components/newslist/Newslist.js
+++ src/components/newslist/Newslist.js
@@ -1,36 +1,48 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Config from "../../Config";
+import { fetchArticles } from "../../modules/news/NewsService";
 
 import commonStyles from "../Common.module.css";
 import styles from "./Newslist.module.css";
 
-import data from "../../data/demo_news.json";
+export default () => {
+  const [articles, setArticles] = useState([]);
 
-const Newslist = () => (
-  <div className={commonStyles.content}>
-    <div className={styles.newslistContainer}>
-      <ul>
-        {data.map(({ id, title, text, date, images }) => (
-          <li>
-            <div className={styles.metadata}>
-              <img src={images[0].src} />
-            </div>
+  if (articles.length === 0) {
+    fetchArticles().then((data) => setArticles(data));
+  }
 
-            <div className={styles.introduction}>
-              <h3>
-                <Link key={id} to={`article/${id}`}>
-                  <span className={styles.headline}>{title}</span>
-                  <span className={styles.subtitle}>{text.substr(20)}</span>
-                </Link>
-              </h3>
-            </div>
-          </li>
-        ))}
-      </ul>
+  return (
+    <div className={commonStyles.content}>
+      <div className={styles.newslistContainer}>
+        <ul>
+          {articles.length
+            ? articles.map(({ id, Title, Text, Date, Images }) => (
+                <li key={id}>
+                  <div className={styles.metadata}>
+                    <img
+                      src={`${Config.contentUrl}${Images[0].formats.thumbnail.url}`}
+                    />
+                  </div>
+
+                  <div className={styles.introduction}>
+                    <h3>
+                      <Link key={id} to={`article/${id}`}>
+                        <span className={styles.headline}>{Title}</span>
+                        <span className={styles.subtitle}>
+                          {Text.substr(20)}
+                        </span>
+                      </Link>
+                    </h3>
+                  </div>
+                </li>
+              ))
+            : ""}
+        </ul>
+      </div>
     </div>
-  </div>
-);
-
-export default Newslist;
+  );
+};
