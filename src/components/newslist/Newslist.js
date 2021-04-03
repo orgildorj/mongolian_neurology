1c6a03722b99506243fd1a69eef18b3d29419bc0
Orgil <orgil.dorj720@gmail.com>
2021-04-03 21:48:23 +0200

    Working on newslist

--- src/components/newslist/Newslist.js
+++ src/components/newslist/Newslist.js
@@ -11,26 +11,49 @@ import data from "../../data/demo_news.json";
 const Newslist = () => (
   <div className={commonStyles.content}>
     <div className={styles.newslistContainer}>
-      <ul>
-        {data.map(({ id, title, text, date, images }) => (
-          <li>
-            <div className={styles.metadata}>
-              <img src={images[0].src} />
-            </div>
-
-            <div className={styles.introduction}>
-              <h3>
-                <Link key={id} to={`article/${id}`}>
-                  <span className={styles.headline}>{title}</span>
-                  <span className={styles.subtitle}>{text.substr(20)}</span>
-                </Link>
-              </h3>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {data.map(({ id, title, text, date, images }) =>
+        NewsSection(id, title, text, date, images)
+      )}
     </div>
   </div>
+);
+
+const NewsSection = (id, title, text, date, images) => (
+  <div className={styles.newsSection}>
+    <div className={styles.header}>
+      <Link key={id} to={`article/${id}`}>
+        <span className={styles.headline}>{title}</span>
+      </Link>
+    </div>
+    <div className={styles.pic}>
+      <figure>
+        <img src={images[0].src} />
+        <figcaption>
+          <date>{date}</date>
+        </figcaption>
+      </figure>
+    </div>
+    <div className={styles.introduction}>
+      <span className={styles.subtitle}>{text}</span>
+    </div>
+  </div>
+
+  /* <div className={styles.newsSection}>
+    <div className={styles.metadata}>
+      <img src={images[0].src} />
+    </div>
+
+    <div className={styles.introduction}>
+      <div>
+        <Link key={id} to={`article/${id}`}>
+          <p>
+            <span className={styles.headline}>{title}</span>
+            <span className={styles.subtitle}>{text}</span>
+          </p>
+        </Link>
+      </div>
+    </div>
+  </div> */
 );
 
 export default Newslist;
